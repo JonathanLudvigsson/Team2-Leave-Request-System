fd5e098e94a35313ef6b8898e2093a2373f066bd
Henrik Rydén <henrik.ryden@hotmail.com>
2023-10-12 11:43:13 +0200

    Added Angular project

---
+++ EmployeeLeave-FrontEnd/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+ 
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path:'user', component: UserComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
