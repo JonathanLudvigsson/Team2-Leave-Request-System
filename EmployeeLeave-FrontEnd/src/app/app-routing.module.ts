484a1d128935a3fa2df6e439c4b4f6ed40d3b855
Henrik Rydén <henrik.ryden@hotmail.com>
2023-10-12 13:48:26 +0200

    Added a simple landing page for login

--- EmployeeLeave-FrontEnd/src/app/app-routing.module.ts
+++ EmployeeLeave-FrontEnd/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
  
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path:'user', component: UserComponent},
+  { path: '', component: LoginComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'home', component: HomeComponent},
 ];
 
 @NgModule({
