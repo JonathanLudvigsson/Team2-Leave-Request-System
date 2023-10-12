a8a4de6d4aa7b5dee1003b86c44d9d327a36156f
Henrik Rydén <henrik.ryden@hotmail.com>
2023-10-12 13:51:02 +0200

    Login

--- EmployeeLeave-FrontEnd/src/app/app.module.ts
+++ EmployeeLeave-FrontEnd/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    UserComponent
+    UserComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule, HttpClientModule, AppRoutingModule
