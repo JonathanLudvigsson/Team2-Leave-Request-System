72085978c8c408e03795ae98660e05e310df70be
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-10-20 13:24:21 +0200

    Email jobs working, hangfire now included. Db update needed.

--- EmployeeLeaveAPI/Interfaces/IEmailService.cs
+++ EmployeeLeaveAPI/Interfaces/IEmailService.cs
@@ -1,6 +1,12 @@
+using EmployeeLeaveAPI.Models;
+
 namespace EmployeeLeaveAPI.Interfaces;
 
 public interface IEmailService
 {
+    Task<(bool isSuccess, string message, Email? email)> CreateEmail(int userId);
+    Task<(bool isSuccess, string message)> SaveEmailToDbAsync(Email email);
+    Task<(bool isSuccess, string message)> EnqueueEmail(Email email);
+    Task<(bool isSuccess, string message)> SendEmail(Email email);
     
 }
