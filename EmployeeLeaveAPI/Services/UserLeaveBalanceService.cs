844a06d19251fb85d7f2e4003d65a7c09d0c7b4b
qvarnstr0m <mqvarnstrom80@gmail.com>
2023-10-24 10:34:32 +0200

    testing

--- EmployeeLeaveAPI/Services/UserLeaveBalanceService.cs
+++ EmployeeLeaveAPI/Services/UserLeaveBalanceService.cs
@@ -38,7 +38,7 @@ public class UserLeaveBalanceService : IUserLeaveBalanceService
             foreach (var leaveType in leaveTypes)
             {
                 var daysTaken = leaveGroups.FirstOrDefault(x => x.LeaveTypeId == leaveType.LeaveTypeID)?.DaysTaken ?? 0;
-                var daysLeft = leaveType.MaximumDays - daysTaken + 99999;
+                var daysLeft = leaveType.MaximumDays - daysTaken + 1;
                 
                 result.Add(new UserLeaveBalanceDTO
                 {
